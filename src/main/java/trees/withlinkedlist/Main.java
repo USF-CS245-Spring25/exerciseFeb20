ac04aaa2c5e74c6926663dbf0540d16c7cbd9efc
okarpenko <okarpenko@usfca.edu>
2025-02-20 01:18:17 -0800

    Lab 20 on general trees with a linked list of children.

---
+++ src/main/java/trees/withlinkedlist/Main.java
@@ -0,0 +1,12 @@
+package trees.withlinkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+        tree.createNodes();
+        tree.printPreorder();
+
+       // FILL IN CODE:
+
+    }
+}
